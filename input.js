23f3d5eee8aafa27a0138ffbc5f4e9c71350d0b6
davemgj84 <davemgj@gmail.com>
2020-08-26 21:37:42 +0000

    Added more to function handleUserInput to handle w,a,s,d user input for snake movement - also corrected some mistakes with my module.exports = { connect }

--- input.js
+++ input.js
@@ -1,5 +1,10 @@
 // Setup User Interface
 // Specifically, so that we can handle user input via stdin
+
+// Had to bring in connect() then name it to a variable so that I could use conn.write for user Input
+const { connect } = require('./client');
+const conn = connect();
+
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -14,9 +19,21 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    conn.write('Move: up');
+  }
+  if (key === 'a') {
+    conn.write('Move: left');
+  }
+  if (key === 's') {
+    conn.write('Move: down');
+  }
+  if (key === 'd') {
+    conn.write('Move: right');
+  }
 };
 
-module.exports = setupInput;
+module.exports = { setupInput };
 
 // stdin.addListener('data', handleUserInput); <---- This also works instead of using stdin.on('data', handleUserInput);
 // not sure if one is prefered over the other - .addListener() seems more human readable compared to .on()
--- play.js
+++ play.js
@@ -1,8 +1,8 @@
 // Multi-Player Snake Game :
-const connect = require('./client');
-const setupInput = require('./input');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-console.log('Connecting ...');
 connect();
+console.log('Connecting ...');
 
 setupInput();
