539e7d23efe2114081032f54ee653728a2935101
davemgj84 <davemgj@gmail.com>
2020-08-26 19:46:04 +0000

    initial client setup for snake game

---
+++ play.js
@@ -0,0 +1,25 @@
+// Multi-Player Snake Game :
+
+const net = require('net');
+
+// Establishes connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  // an event handler to handle incoming data.
+  // console log it for the player
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
